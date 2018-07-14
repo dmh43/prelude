57cb49d964c994ca88791cbf5a0135f2ba39f83e
Dany Haddad <danyhaddad43@gmail.com>
2018-07-13 19:05:08 -0500

    use conda env

--- personal/python-config.el
+++ personal/python-config.el
@@ -2,4 +2,14 @@
 
 (setf python-indent-offset 2)
 
+(require 'conda)
+(conda-env-autoactivate-mode t)
+(setf conda-anaconda-home "/anaconda3")
+(conda-env-initialize-interactive-shells)
+(conda-env-initialize-eshell)
+(setq python-shell-interpreter "ipython"
+      python-shell-interpreter-args "--simple-prompt -i ")
+
+(setq-default mode-line-format (cons '(:exec conda-env-current-name) mode-line-format))
+
 (provide 'python-config)
